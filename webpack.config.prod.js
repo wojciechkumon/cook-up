49f9dfa9b4421036553c7038aaa361af7ce70f1e
Wojciech Kumoń <wojtas626@gmail.com>
2017-04-16 10:30:44 +0200

    Added babel property initializer syntax support to prod build

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -27,7 +27,8 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ["es2015", "react"]
+            presets: ["es2015", "react"],
+            plugins: ["transform-class-properties"]
           }
         },
       },
