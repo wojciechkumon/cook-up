fb9a197190977175ddc266785843f86a719e1594
ewelinarybczynska <e.rybczynska@gmail.com>
2017-04-30 11:29:51 +0200

    Finder table style

--- src/main/web/finder/js/RecipeListRow.js
+++ src/main/web/finder/js/RecipeListRow.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import PropTypes from "prop-types";
-import { withRouter } from 'react-router-dom'
+import { withRouter } from 'react-router-dom';
+import "../style/RecipeListRow.scss";
 
 class RecipeListRow extends Component {
 
