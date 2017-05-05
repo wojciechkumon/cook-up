d8dfae0e5ba75d870786d6fcc85e5cd7984a724c
Wojciech Kumoń <wojtas626@gmail.com>
2017-05-05 18:27:49 +0200

    Comments fetch error handling

--- src/main/web/main/js/actions/actions.js
+++ src/main/web/main/js/actions/actions.js
@@ -2,5 +2,8 @@ export const HTTP_NOT_FOUND = 'HTTP_NOT_FOUND';
 export const HTTP_ERROR = 'HTTP_ERROR';
 
 export function getHttpError(response) {
-  return response && response.status.code === 404 ? HTTP_NOT_FOUND : HTTP_ERROR;
+  return response
+         && response.status
+         && response.status.code === 404
+    ? HTTP_NOT_FOUND : HTTP_ERROR;
 }
