d18b990b278eda2c589e9a01e6bc990ead4ba4f6
Wojciech Kumoń <wojtas626@gmail.com>
2017-05-10 12:54:47 +0200

    redux-logger will be imported only in dev env, reduced prod bundle.js size

--- src/main/web/main/js/index.js
+++ src/main/web/main/js/index.js
@@ -2,13 +2,12 @@ import React from "react";
 import {render} from "react-dom";
 import {applyMiddleware, createStore} from "redux";
 import thunkMiddleware from "redux-thunk";
-import {createLogger} from "redux-logger";
 import Root from "./Root";
 import cookUpApp from "./reducers/cookUpApp";
 import {setUserIfLoggedIn} from "../../header/js/loginSubmitter";
 
 const middleware = process.env.NODE_ENV !== 'production' ?
-    applyMiddleware(thunkMiddleware, createLogger()) :
+    applyMiddleware(thunkMiddleware, require("redux-logger").createLogger()) :
     applyMiddleware(thunkMiddleware);
 
 const store = createStore(cookUpApp, middleware);
