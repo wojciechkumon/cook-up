7caafa081b73eb3bdd5afff6a8ffe1c5dd132fb0
Wojciech Kumoń <wojtas626@gmail.com>
2017-05-21 14:57:08 +0200

    White font optimization (css)

--- src/main/web/me/js/MePage.js
+++ src/main/web/me/js/MePage.js
@@ -1,10 +1,21 @@
 import React, {Component} from "react";
+import RecipeList from "../../finder/js/RecipeList";
 
 class MePage extends Component {
 
   render() {
     return (
-      <div>It's me!</div>
+      <div>
+        <span>It's me!</span>
+        <div>
+          <h3>My recipes</h3>
+          <RecipeList recipes={[]}/>
+        </div>
+        <div>
+          <h3>Favourite recipes</h3>
+          <RecipeList recipes={[]}/>
+        </div>
+      </div>
     );
   }
 }
