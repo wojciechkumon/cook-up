c6ff48d247c3b32fa825a6d6ecbf0daf515ddb8e
ewelinarybczynska <e.rybczynska@gmail.com>
2017-05-23 13:27:44 +0200

    author name me page

--- src/main/web/me/js/MePage.js
+++ src/main/web/me/js/MePage.js
@@ -1,13 +1,17 @@
 import React, {Component} from "react";
+import {connect} from "react-redux";
+import PropTypes from "prop-types";
 import MyRecipes from "./MyRecipes";
 import FavouriteRecipes from "./FavouriteRecipes";
-import "./style/MePage.scss";
+import "../style/MePage.scss";
 
 class MePage extends Component {
 
   render() {
+    const {author} = this.props;
     return (
       <div className="MePage">
+        <h4>{author}</h4>
         <MyRecipes/>
         <FavouriteRecipes/>
       </div>
@@ -15,4 +19,14 @@ class MePage extends Component {
   }
 }
 
-export default MePage;
+MePage.propTypes = {
+  author: PropTypes.string.isRequired,
+};
+
+const mapStateToProps = state => {
+  return {
+    author: state.auth.email
+  }
+};
+
+export default connect(mapStateToProps)(MePage);
