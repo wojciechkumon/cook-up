50c9bcd506e42f0cae4f95eaf97eda1b1eb7447a
Wojciech Kumoń <wojtas626@gmail.com>
2017-05-31 09:07:47 +0200

    Edit recipe page added

---
+++ src/main/web/editRecipe/js/RecipeEditor.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+
+class RecipeEditor extends Component {
+
+  render() {
+    return (
+      <div>
+        <h4>Edit recipe</h4>
+      </div>
+    );
+  }
+}
+
+export default RecipeEditor;
